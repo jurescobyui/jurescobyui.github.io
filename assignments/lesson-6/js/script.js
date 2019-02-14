d20ab690696328d8497484aec015b9a9ec16e060
jurescobyui <esc18011@byui.edu>
2019-02-15 06:47:48 +0800

    partial

--- assignments/lesson-6/js/script.js
+++ assignments/lesson-6/js/script.js
@@ -1,20 +1,4 @@
-//get the window width and heght
-//my simple code
-function getWindowSize() {
-    var w = window.innerWidth;
-    //var h = window.innerHeight;
-    //alert(w + " " + h);
-    document.getElementById("winSize").innerHTML = "Width: " + w ;
-  }
-
-// code from : https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onresize
-//  const heightOutput = document.querySelector('#height');
-//const widthOutput = document.querySelector('#width');
-
-/*function resize() {
- // heightOutput.textContent = window.innerHeight;
-  widthOutput.textContent = window.innerWidth;
-}*/
-
-//window.onresize = resize;
-window.onresize = getWindowSize;
+function toggleMenu(){
+  //console.log(document.getElementById('primaryNav').classList);
+  document.getElementById('primaryNav').classList.toggle("hide");
+}
