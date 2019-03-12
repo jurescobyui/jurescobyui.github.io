9063667d74d5921cd2f44a76e9864c5850c749c3
jurescobyui <esc18011@byui.edu>
2019-03-12 12:49:49 +0800

    updated weather js again

--- assignments/lesson-9/js/weatherapi.js
+++ assignments/lesson-9/js/weatherapi.js
@@ -9,5 +9,5 @@ request.onload = function() {
     console.log(weatherRequest.responseText);
 }
 //my api key: APPID=8ec39d712acbcc119f2f75333221c559
-
+//http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ec39d712acbcc119f2f75333221c559
 //sample api.openweathermap.org/data/2.5/weather?id=2172797
