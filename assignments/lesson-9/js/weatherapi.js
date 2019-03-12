9d2f9659350fc620a87dad3b05803837d1cfd53e
jurescobyui <esc18011@byui.edu>
2019-03-12 12:20:51 +0800

    update weatherapi.js

--- assignments/lesson-9/js/weatherapi.js
+++ assignments/lesson-9/js/weatherapi.js
@@ -4,7 +4,8 @@ function reqListener () {
 let weatherRequest = new XMLHttpRequest();
 
 weatherRequest.addEventListener("load", reqListener);
-weatherRequest.open("GET", "api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=8ec39d712acbcc119f2f75333221c559");
+weatherRequest.open("GET", "api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ec39d712acbcc119f2f75333221c559");
 weatherRequest.send();
 //my api key: APPID=8ec39d712acbcc119f2f75333221c559
 
+//sample api.openweathermap.org/data/2.5/weather?id=2172797
