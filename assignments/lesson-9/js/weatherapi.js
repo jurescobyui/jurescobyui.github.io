a23c79405d616da4b3cc39f42231e05fb412cf35
jurescobyui <esc18011@byui.edu>
2019-03-12 15:22:36 +0800

    update weatherapi.js

--- assignments/lesson-9/js/weatherapi.js
+++ assignments/lesson-9/js/weatherapi.js
@@ -1,19 +1,17 @@
-function reqListener () {
-    console.log(this.responseText);
-  }
 let weatherRequest = new XMLHttpRequest();
-weatherRequest.addEventListener("load", reqListener);
+var apiURLstring = "http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ec39d712acbcc119f2f75333221c559";
 //working api for openweather
-//weatherRequest.open("GET", "http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ec39d712acbcc119f2f75333221c559");
-// test api on techdb.win preston json
-weatherRequest.open("GET", "http://techdb.win/files/byui/cit230/preston.json");
+weatherRequest.open("GET",apiURLstring,true);
 //weatherRequest.responseType = 'json';
 weatherRequest.send();
-/*
+
 weatherRequest.onload = function() {
-    //var townData = request.response;
-    console.log(weatherRequest.responseXML);
-}*/
-//my api key: APPID=8ec39d712acbcc119f2f75333221c559
-//http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ec39d712acbcc119f2f75333221c559
-//sample api.openweathermap.org/data/2.5/weather?id=2172797
+    //assign response to var jsonObj
+//var jsonObj = weatherRequest.response;
+let weatherData = JSON.parse(weatherRequest.responseText);
+console.log(weatherData);
+    //get current temp from json array assign to var curTemp
+var curTemp = weatherData.main.temp;
+    //output the current temp in the html
+   document.getElementById('current-temp').innerHTML=curTemp;
+}
