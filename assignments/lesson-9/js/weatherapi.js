8097b457f4b8e4d8774e50074d0e8dfd29ffd3f1
jurescobyui <esc18011@byui.edu>
2019-03-12 14:30:23 +0800

    added preston json to lesson 10

--- assignments/lesson-9/js/weatherapi.js
+++ assignments/lesson-9/js/weatherapi.js
@@ -1,13 +1,19 @@
-
+function reqListener () {
+    console.log(this.responseText);
+  }
 let weatherRequest = new XMLHttpRequest();
-weatherRequest.responseType = 'json';
-//weatherRequest.addEventListener("load", reqListener);
-weatherRequest.open("GET", "api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ec39d712acbcc119f2f75333221c559");
+weatherRequest.addEventListener("load", reqListener);
+//working api for openweather
+//weatherRequest.open("GET", "http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ec39d712acbcc119f2f75333221c559");
+// test api on techdb.win preston json
+weatherRequest.open("GET", "http://techdb.win/files/byui/cit230/preston.json");
+//weatherRequest.responseType = 'json';
 weatherRequest.send();
-
-request.onload = function() {
-    console.log(weatherRequest.responseText);
-}
+/*
+weatherRequest.onload = function() {
+    //var townData = request.response;
+    console.log(weatherRequest.responseXML);
+}*/
 //my api key: APPID=8ec39d712acbcc119f2f75333221c559
 //http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=8ec39d712acbcc119f2f75333221c559
 //sample api.openweathermap.org/data/2.5/weather?id=2172797
