c8e588a25df01315aa0fc2aa0331ef27c517c8c4
jurescobyui <esc18011@byui.edu>
2019-03-22 20:27:58 +0800

    update js

--- assignments/weathersite/js/events.js
+++ assignments/weathersite/js/events.js
@@ -10,16 +10,12 @@ var request = new XMLHttpRequest();
         var townData = request.response;
        for(var i=0; i < townData.towns.length; i++){
          if(townData.towns[i].name===cityName){
-                var myH3 = document.createElement('h3');//declare new h3
-                myH3.innerText="Events";// townData.towns[i].name; // assign a text in the h3 element
-                divFlex.appendChild(myH3);// add the element to the dom
-
-                var myP1=document.createElement('p');// declare a new p element
-               
-              
+               var myH3 = document.createElement('h3');//declare new h3
+               myH3.innerText="Events";// townData.towns[i].name; // assign a text in the h3 element
+               divFlex.appendChild(myH3);// add the element to the dom
+               var myP1=document.createElement('p');// declare a new p element              
                var myUL=document.createElement('ul');
-               
-                
+            
                for(var j=0;j<townData.towns[i].events.length; j++){
                 var myLi=document.createElement('li');
                   myLi.innerHTML = townData.towns[i].events[j];
@@ -28,9 +24,7 @@ var request = new XMLHttpRequest();
                }
                myP1.appendChild(myUL);
                divFlex.appendChild(myP1);
-              
-               
-                
+
                 console.log(i + " " + townData.towns[i].name);
         
             }// end of if
