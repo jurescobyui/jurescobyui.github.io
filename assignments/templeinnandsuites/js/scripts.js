7527cc1ca5cf0f15a3f11b8951eaadb13c6d153b
jurescobyui <esc18011@byui.edu>
2019-04-03 22:18:57 +0800

    another update of menu and hero image

---
+++ assignments/templeinnandsuites/js/scripts.js
@@ -0,0 +1,20 @@
+
+function myFunction() {
+    var x = document.getElementById("mytopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+    } else {
+    x.className = "topnav";
+    }
+}
+
+function hideme(){
+    var y = document.getElementById("drpdown-content");
+    if(y.className==="dropdown-content"){
+        y.className +=" hidden"
+    }else{
+        y.className="dropdown-content";
+    }
+}
+
+        
