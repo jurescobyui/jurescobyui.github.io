3d0d76ff2452b0c297bffcbdae45042323832e36
jurescobyui <esc18011@byui.edu>
2019-04-12 11:28:29 +0800

    updated json file

--- assignments/templeinnandsuites/js/templesapi.js
+++ assignments/templeinnandsuites/js/templesapi.js
@@ -18,6 +18,43 @@ switch(templeName){
         break;
 }
 
+//get temple data from json file
+let templeRequest = new XMLHttpRequest();
+let xhrURLString = "https://jurescobyui.github.io/assignments/templeinnandsuites/js/temples.json";
+templeRequest.open("GET", xhrURLString,true);
+//templeData.responseType='json';
+templeRequest.send();
+templeRequest.onload=function(){
+let templeData = JSON.parse(templeRequest.responseText);
+
+console.log(templeData);
+console.log(templeData.length);
+var txtServices=" ";
+for(i=0;i<templeData.length;i++){
+    console.log(templeData[i].name);
+    if(templeData[i].name===templeName){
+        document.getElementById('templeImage').src=templeData[i].image;
+        document.getElementById('templeAddress').innerHTML=templeData[i].address;
+        document.getElementById('tel').innerHTML=templeData[i].tel;
+        for(j=0;j<templeData[i].services.length;j++){
+           // txtServices += templeData[i].services[j] +"\n";
+           console.log(templeData[i].services[j]);
+        }
+       // document.getElementById('templeServices').innerHTML=txtServices;
+       
+        console.log(templeData[i].address);
+        console.log(templeData[i].tel);
+        console.log(templeData[i].services);
+        console.log(templeData[i].closures);
+        console.log(templeData[i].milestones);
+        
+    }
+}
+
+
+}//end templeData onload funcion
+
+// pull out weather for the temple location
 let weatherRequest = new XMLHttpRequest();
 var apiURLstring = "https://api.openweathermap.org/data/2.5/weather?lon="+lon+"&lat="+lat+"&units=imperial&APPID=8ec39d712acbcc119f2f75333221c559";
 
